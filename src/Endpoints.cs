044c67eb5ae0f0599900a85cf1e8043d1d2c0565
Luc4sKr <lucaskreuch77@gmail.com>
2024-03-09 17:28:04 -0300

    update

--- src/Endpoints.cs
+++ src/Endpoints.cs
@@ -1,4 +1,6 @@
+using System.Transactions;
 using Dapper;
+using Microsoft.Data.SqlClient;
 using Npgsql;
 using RinhaDeBackend.Models;
 
@@ -6,25 +8,50 @@ namespace RinhaDeBackend
 {
     public static class Endpoints
     {
-        private static int[] user_ids = new int[] {1, 2, 3, 4, 5};
-        
+        private static readonly int[] user_ids = new int[] { 1, 2, 3, 4, 5 };
+
         public static async Task<IResult> Transacoes(int id, TransacaoRequest transacaoRequest, string conn)
         {
             if (!user_ids.Contains(id))
                 return Results.NotFound();
-            
-            using (var connection = new NpgsqlConnection(conn))
-            {
-                var sqlSaldo = $"SELECT * FROM saldo WHERE user_id = id";
-                var saldo = await connection.QueryFirstOrDefaultAsync<SaldoResponse>(sqlSaldo);
-                
-                if (transacaoRequest.tipo ==  'd')
-                {
-                    if (saldo.total - transacaoRequest.valor < saldo.limite)
-                        return Results.UnprocessableEntity();
-                }
 
-                return Results.Ok();
+            await using (var connection = new NpgsqlConnection(conn))
+            {
+                await connection.OpenAsync();
+                await using (NpgsqlTransaction transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        var sqlSaldo =
+                            $"SELECT total, limite, CURRENT_DATE AS data_extrato FROM saldos WHERE user_id = {id}";
+                        var saldo = await connection.QueryFirstOrDefaultAsync<SaldoResponse>(sqlSaldo);
+
+                        if (!ValidarTransacao(transacaoRequest, saldo))
+                            return Results.UnprocessableEntity();
+
+                        var sqlInsert =
+                            $@"INSERT INTO transactions (user_id, valor, tipo, descricao, realizada_em) 
+                                    VALUES (@UserId, @Valor, @Tipo, @Descricao, @RealizadaEm)";
+                        var parametros = new { UserId = id, Valor = transacaoRequest.valor, Tipo = transacaoRequest.tipo, Descricao = transacaoRequest.descricao, RealizadaEm = DateTime.Now };
+                        await connection.ExecuteAsync(sqlInsert, parametros, transaction);
+                        
+                        // Calcular novo saldo
+                        var novoSaldo = CalcularNovoSaldo(saldo.total, transacaoRequest.valor, transacaoRequest.tipo);
+
+                        // Atualizar o saldo do cliente na tabela saldos
+                        var sqlUpdateSaldo = "UPDATE saldos SET total = @NovoSaldo WHERE user_id = @UserId";
+                        await connection.ExecuteAsync(sqlUpdateSaldo, new { novoSaldo, UserId = id }, transaction);
+
+                        transaction.Commit();
+                        
+                        return Results.Ok(new { Limite = saldo.limite, Saldo = novoSaldo });
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        return Results.NotFound(ex.Message);
+                    }
+                }
             }
         }
 
@@ -32,17 +59,45 @@ namespace RinhaDeBackend
         {
             if (!user_ids.Contains(id))
                 return Results.NotFound();
-            
-            using (var connection = new NpgsqlConnection(conn))
-            {
-                var sqlSaldo = $"SELECT total, limite, CURRENT_DATE AS data_extrato FROM saldos WHERE user_id = {id}";  
-                var saldo = await connection.QueryFirstOrDefaultAsync<SaldoResponse>(sqlSaldo);
 
-                var sqlTransacoes = $"SELECT valor, tipo, descricao, realizada_em FROM transactions WHERE user_id = {id} ORDER BY id DESC LIMIT 10";
-                var transacoes = await connection.QueryAsync<UltimasTransacoesResponse>(sqlTransacoes);
-                
-                return Results.Ok(new ExtratoResponse(saldo, transacoes.ToList()));
+            await using (var connection = new NpgsqlConnection(conn))
+            {
+                try
+                {
+                    var sqlSaldo =
+                        $"SELECT total, limite, CURRENT_DATE AS data_extrato FROM saldos WHERE user_id = {id}";
+                    var saldo = await connection.QueryFirstOrDefaultAsync<SaldoResponse>(sqlSaldo);
+
+                    var sqlTransacoes =
+                        $"SELECT valor, tipo, descricao, realizada_em FROM transactions WHERE user_id = {id} ORDER BY id DESC LIMIT 10";
+                    var transacoes = await connection.QueryAsync<UltimasTransacoesResponse>(sqlTransacoes);
+                    return Results.Ok(new ExtratoResponse(saldo, transacoes.ToList()));
+                }
+                catch (Exception ex)
+                {
+                    return Results.NotFound(ex.Message);
+                }
             }
         }
+
+        #region methods
+        private static bool ValidarTransacao(TransacaoRequest request, SaldoResponse saldo)
+        {
+            if (request.tipo == 'd' && saldo.total - request.valor < -saldo.limite)
+                return false;
+
+            return true;
+        }
+        
+        private static decimal CalcularNovoSaldo(decimal saldoAtual, decimal valorTransacao, char tipoTransacao)
+        {
+            if (tipoTransacao == 'c')
+                return saldoAtual + valorTransacao;
+            if (tipoTransacao == 'd')
+                return saldoAtual - valorTransacao;
+
+            return saldoAtual;
+        }
+        #endregion
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -3,20 +3,16 @@ using RinhaDeBackend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddNpgsqlDataSource(
-    Environment.GetEnvironmentVariable(
-        "Host=localhost;Username=postgres;Password=admin;Database=rinha;Minimum Pool Size=10;Maximum Pool Size=10;Multiplexing=true;Application Name=RinhaDeBackendDotnet") ??
-        "ERRO de connection string!");
+var conn = builder.Configuration.GetConnectionString("DB_CONNECTION_STRING") ?? "ERRO de connection string!";
 
-string _conn = "Host=localhost;Username=postgres;Password=admin;Database=rinha;Minimum Pool Size=10;Maximum Pool Size=10;Multiplexing=true;Application Name=RinhaDeBackendDotnet";
-
+builder.Services.AddNpgsqlDataSource(conn);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // TODO
-app.MapPost("/clientes/{id}/transacoes", async (int id, TransacaoRequest transacaoRequest) =>  await Endpoints.Transacoes(id, transacaoRequest, _conn));
-app.MapGet("/clientes/{id}/extrato", async (int id) => await Endpoints.Extrato(id, _conn)).Produces<string>();
+app.MapPost("/clientes/{id}/transacoes", async (int id, TransacaoRequest transacaoRequest) =>  await Endpoints.Transacoes(id, transacaoRequest, conn));
+app.MapGet("/clientes/{id}/extrato", async (int id) => await Endpoints.Extrato(id, conn)).Produces<string>();
 
 app.Run();
